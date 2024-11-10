39580cabad625ffdb5c52f5ff609b8b4e938b8db
Lirirum <prime102102@gmail.com>
2024-11-10 20:47:37 +0200

    First commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter , Routes } from '@angular/router';
+
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {SecretComponent} from "./secret/secret.component";
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+const appRoutes: Routes =[
+  { path: "", component: HomeComponent},
+  { path: "about", component: AboutComponent},
+  { path: "secret", component: SecretComponent},
+];
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes), provideClientHydration()]
 };
